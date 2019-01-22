c21f73c4501d4bb488a97b360003b7bbc4e24619
Nao Pross <naopross@thearcway.org>
2019-01-22 01:52:21 +0100

    Re-implement flattask.h in task.hpp
    Note: On this commit NOTHING compiles.

---
+++ engine/include/task.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <functional>
+#include <set>
+
+namespace flat {
+    namespace core {
+        class task {
+        public:
+            enum class priority : unsigned {
+                max = 0,
+                high = 1,
+                none = 2,
+                low = 3,
+                min = 4,
+            };
+
+            // to pass a member function (method) use std::bind(f, obj)
+            task(std::function<void()> callback, priority p = priority::none);
+            task() = delete;
+
+            inline void operator()() const { m_callback(); }
+            friend bool operator<(const task& lhs, const task& rhs);
+
+        private:
+            const priority m_priority;
+            std::function<void()> m_callback;
+        };
+
+        struct job : public std::set<task> {
+            inline void add_task(task t) {
+                insert(begin(), t);
+            }
+
+            void invoke_tasks();
+        };
+    }
+}
