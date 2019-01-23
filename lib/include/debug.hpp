6ca79425af17c10f016545231b101f8ad8207ecd
Nao Pross <naopross@thearcway.org>
2019-01-23 21:04:25 +0100

    Add powerful debug header
    Also, delete lib/libwrapsdl2. Why is it still there?

---
+++ lib/include/debug.hpp
@@ -0,0 +1,46 @@
+#pragma once
+
+#ifdef DEBUG
+#include <iostream>
+#include <sstream>
+
+#define __FILENAME__ (\
+    __builtin_strrchr(__FILE__, '/') ? \
+    __builtin_strrchr(__FILE__, '/') + 1 : __FILE__)
+
+#define npdebug(...); { \
+    std::cerr << "[" << __FILENAME__ \
+              << ":" << __LINE__ \
+              << ", " << __func__ \
+              << "] " ; \
+    _np::debug(__VA_ARGS__); \
+}
+
+#define npinspect(...); _np::inspect(__VA_ARGS__);
+
+namespace _np {
+    template<typename... Args>
+    inline void debug(const Args&... args) {
+        (std::cerr << ... << args) << std::endl;
+    }
+
+    template<typename Msg, typename Expr, typename... Args>
+    inline T& inspect(const Msg& msg, Expr& expr, const Args&... args) {
+        npdebug(msg, expr, args);
+        return expr;
+    }
+}
+
+#else
+
+#define npdebug(...); {}
+#define npinspect(...); {}
+
+namespace _np {
+    template<typename.. Args>
+    void debug(Args&... args) {}
+
+    template<typename Msg, typename Expr, typename... Args>
+    inline T& inspect(const Msg& msg, Expr& expr, const Args&... args) {}
+}
+#endif
