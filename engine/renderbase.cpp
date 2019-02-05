210440b6e6141823fd92ba13510c7f74874c0857
ancarola <raffaele.ancarola@epfl.ch>
2019-02-05 09:54:51 +0100

    Update on renderbase, channel::emit(Args...)

--- engine/renderbase.cpp
+++ engine/renderbase.cpp
@@ -7,14 +7,14 @@ using namespace flat;
 renderbase::renderbase(const std::string& id) : core::labelled(id)
 {
     // TODO, signal creation
-    core::signal sig(shared_from_this(), true);
-    flat::core_channel()->emit(std::move(sig));
+    //core::signal sig(shared_from_this(), true);
+    flat::core_channel()->emit(shared_from_this(), true);
 }
 
 renderbase::~renderbase()
 {
     // TODO, signal destruction
-    core::signal sig(shared_from_this(), false);
-    flat::core_channel()->emit(std::move(sig));
+    //core::signal sig(shared_from_this(), false);
+    flat::core_channel()->emit(shared_from_this(), false);
 }
 
