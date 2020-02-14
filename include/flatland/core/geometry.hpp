00a54f15323ecf2e5f78161fb1e55e6131a4c1f1
DaedricSpartan99 <leleraffa97@hotmail.it>
2020-02-14 12:38:44 +0100

    core/geometry.hpp added

--- include/flatland/core/geometry.hpp
+++ include/flatland/core/geometry.hpp
@@ -3,28 +3,50 @@
 #include <mm/mmvec.hpp>
 
 namespace flat::geom {
+
+    template <typename T>
+    using corner = mm::vec2<T>;
+
+    // define comparison operator overloading strictly in this namespace
+    // WARNING: a < b doesn't mean !(b < a)
+    inline bool operator<(const corner& a, const corner& b) {
+        return a[0] < b[0] && a[1] < b[1];
+    }
+
+    inline bool operator>(const corner& a, const corner& b) {
+        return a[0] > b[0] && a[1] > b[1];
+    }
+
     /*
     * Rectangle struct n
     */
     template <typename T>
     struct rect {
-        rect(const mm::vec2<T>& _upper, const mm::vec2<T>& _lower) : upper(_upper), lower(_lower) {}
-        constexpr rect(mm::vec2<T> _upper, mm::vec2<T> _lower) : upper(_upper), lower(_lower) {}
+        rect(const corner& _upper, const corner& _lower) : upper(_upper), lower(_lower) {}
+        constexpr rect(corner _upper, corner _lower) : upper(_upper), lower(_lower) {}
 
-        mm::vec2<T> upper, lower; 
+        corner upper, lower; 
 
         double perimeter() const {
             auto d = r.upper - r.lower;
             return 2 * (d[0] + d[1]);
         }
 
-        bool inscribed_in(const rect<T>& ext) const
+        inline bool inscribed_in(const rect<T>& ext) const
         {
-            return ext.lower[0] < lower[0] &&
-                ext.lower[1] < lower[1] &&
-                upper[0] < ext.upper[0] &&
-                upper[1] < ext.upper[1];
+            return ext.lower < lower && upper < ext.upper;
         }
+
+        inline bool inscribes(const rect<T>& ext) const {
+            return ext.lower > lower && upper > ext.upper;
+        }
+
+        inline bool inside(const corner& c) const {
+            return lower < c && upper > c;
+        }
+
+        // check for overlaps
+        bool overlaps(const rect<T>& ext) const;
     };
 
     // construct a rectangle which embeds both rectangles
@@ -32,5 +54,15 @@ namespace flat::geom {
     rect<T> rect_union(const rect<T>& a, const rect<T>& b) {
         return rect({std::min(a.lower[0], b.lower[0]), std::min(a.lower[1], b.lower[1])}, 
                 {std::max(a.lower[0], b.lower[0]), std::max(a.lower[1], b.lower[1])});
+    }
+
+    /*
+    * TODO, Optimize the double case into the graphic card with matrices
+    */
+    template <typename T>
+    inline bool rect<T>::overlaps(const rect<T>& ext) const
+    {
+        return (inside(ext.lower) && !inside(ext.upper)) ||
+               (inside(ext.upper) && !inside(ext.lower));
     }
 }
